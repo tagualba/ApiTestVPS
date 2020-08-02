c8be2fa029e55610fedf5945356268923b1a0ad3
tagualba <50918251+tagualba@users.noreply.github.com>
2020-08-02 14:48:52 -0300

    Add files via upload

---
+++ TestApiVPS/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestApiVPS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("get")]
+        [HttpGet]
+        public string Get()
+        {
+            return "Llegaste Rey <3";
+        }
+    }
+}
